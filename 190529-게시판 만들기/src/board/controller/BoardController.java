aafcd562e6a12aa0e4f08e9822668f77f42978ec
쭌프로 <42495072+alalstjr@users.noreply.github.com>
2019-05-30 19:16:59 +0900

    Add files via upload

--- 190529-게시판 만들기/src/board/controller/BoardController.java
+++ 190529-게시판 만들기/src/board/controller/BoardController.java
@@ -1,12 +1,14 @@
 package board.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,7 +33,7 @@ public class BoardController extends HttpServlet {
 		String url = request.getRequestURL().toString();
 		BoardDAO dao = new BoardDAO();
 		
-		if(url.indexOf("board_list.do") != -1) 
+		if(url.indexOf("list.do") != -1) 
 		{
 			List<BoardDTO> list = dao.list(); // 게시물 목록이 넘어옴
 			// 출력페이지에서 사용할 수 있도록 request 영역에 저장
@@ -102,7 +104,224 @@ public class BoardController extends HttpServlet {
 			dao.insert(dto); // 레코드가 추가됨
 			// 게시물 목록 갱신
 			String page = 
-					request.getContextPath() + "/board_servlet/board_list.do";
+					request.getContextPath() + "/board_servlet/list.do";
+			response.sendRedirect(page);
+		}
+		else if(url.indexOf("download.do") != -1) 
+		{
+			int num = Integer.parseInt(request.getParameter("num"));
+			String fileName = dao.getFileName(num);
+			// fileName = new String(fileName.getBytes("utf-8"), "8859_1");
+			System.out.println(fileName);
+			
+			// 파일 다운로드 처리
+			String path = Constants.UPLOAD_PATH + fileName;	// 파일의 위치
+			byte b[] = new byte[4096]; 
+			// 파일을 읽기 위한 입력 스트림
+			FileInputStream fis = new FileInputStream(path);
+			// 파일의 종류(마임타입)
+			String mimeType = getServletContext().getMimeType(path);
+			if(mimeType == null) 
+			{ // 다운로드할 파일의 형식
+				mimeType = "application/octet-stream;charset=utf-8";
+			}
+			
+			// 무엇이 전송되는지 명시해주는 헤더 전송, attachment(첨부파일)
+			response.setHeader("Content-Disposition", "attachment;filename="+fileName);
+			
+			// body 전송
+			// 출력 스트림 생성
+			ServletOutputStream out = response.getOutputStream();
+			int numRead;
+			while(true)
+			{
+				numRead =fis.read(b, 0, b.length); // 파일을 읽음
+				if(numRead == -1) break; // 내용이 있으면
+				out.write(b, 0, numRead); // 파일저장
+			}
+			out.flush(); // 스트림 비우기
+			out.close(); // 출력스트림 닫기
+			fis.close(); // 입력스트림 닫기
+			
+			dao.plusDown(numRead);
+		}
+		else if(url.indexOf("view.do") != -1) 
+		{
+			int num = Integer.parseInt(request.getParameter("num"));
+			// 조회수 증가 처리
+			dao.plusReadCount(num, request.getSession());
+			
+			// view 페이지의 경우 줄바꿈 처리를 해줘야 하기때문에 true 를 전달
+			BoardDTO dto = dao.view(num, true);
+			System.out.println("상세화면 dto" + dto);
+			// 출력을 위해 request 영역에 저장
+			request.setAttribute("dto", dto);
+			//출력 페이지로 이동
+			String page = "/board/board_view.jsp";
+			RequestDispatcher rd = request.getRequestDispatcher(page);
+			rd.forward(request, response);
+			
+		}
+		else if(url.indexOf("pass_check.do") != -1) 
+		{
+			int num = Integer.parseInt(request.getParameter("num"));
+			String password = request.getParameter("password");
+			// 올바른 비밀번호인지 확인
+			String result = dao.passwordCheck(num, password);
+			System.out.println("비밀번호 체크결과" + result);
+			String page = new String();
+			// 비번이 맞으면 수정 화면으로 이동
+			if(result != null) 
+			{
+				page = "/board/board_edit.jsp";
+				// edit 페이지의 경우 줄바꿈 처리가 필요없음으로 false 를 전달
+				request.setAttribute("dto", dao.view(num, false));
+				RequestDispatcher rd = request.getRequestDispatcher(page);
+				rd.forward(request, response);
+			}
+			else 
+			{
+				page = request.getContextPath() + 
+						"/board_servlet/view.do?num="+num+"&message=error";
+				response.sendRedirect(page);
+			}
+			
+			// 비번이 틀리면 되돌아감
+		}
+		else if(url.indexOf("update.do") != -1) 
+		{
+			// 폼에서 입력한 값을 dto에 저장
+			BoardDTO dto = new BoardDTO();
+			
+			// 첨부파일처리를 위한 MutipartRequest 선언
+			MultipartRequest multi = new MultipartRequest(
+					request, 
+					Constants.UPLOAD_PATH, 
+					Constants.MAX_UPLOAD, "utf-8",
+					new DefaultFileRenamePolicy());
+			
+			String fileName = new String();
+			int fileSize = 0;
+			try 
+			{
+				// 첨부파일이 여러개일수도 있으니 첨부파일 집합  
+				Enumeration files = multi.getFileNames(); 
+				while (files.hasMoreElements()) 
+				{
+					// 첨부파일의 이름을 불러옵니다.
+					String file1 = (String) files.nextElement();
+					// 시스템 파일 이름을 불러옵니다.
+					fileName = multi.getFilesystemName(file1);
+					// 해당 파일의 사이즈를 구합니다.
+					File f1 = multi.getFile(file1);
+					// 첨부파일의 size
+					if(f1 != null) 
+					{
+						fileSize = (int)f1.length();
+					}
+				}
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+			
+			// reqest -> multi로 변경
+			int num = Integer.parseInt(multi.getParameter("num"));
+			String writer = multi.getParameter("writer");
+			String subject = multi.getParameter("subject");
+			String content = multi.getParameter("content");
+			String password = multi.getParameter("password");
+			String ip = request.getRemoteAddr(); // 클라이언트의 ip주소
+			
+			dto.setNum(num);
+			dto.setWriter(writer);
+			dto.setSubject(subject);
+			dto.setContent(content);
+			dto.setPassword(password);
+			dto.setIp(ip);
+			
+			/*
+			 *	기존 첨부파일이 있을 때
+			 *		파일 삭제 체크 여부
+			 *		새로운 첨부파일이 있을 때 (교체)
+			 *		새로운 첨부파일이 없을 때
+			 *	
+			 *	// 기존 첨푸파일 추가는 문제가 없음
+			 *	기존 첨부파일이 없을 때
+			 *		새로운 첨부파일이 있을 때
+			 *		새로운 첨부파일이 없을 때
+			 * */
+			if(fileName == null || fileName.trim().equals(""))
+			{	// 새로운 첨부파일이 없을 때(기존의 테이블 정보를 가져옴)
+				BoardDTO dto2 = dao.view(num, false);
+				String fName = dto2.getFilename();
+				int fSize = dto2.getFilesize();
+				int fDown = dto2.getDown();
+				dto.setFilename(fName);
+				dto.setFilesize(fSize);
+				dto.setDown(fDown);
+			} 
+			else 
+			{	// 새로운 첨부파일이 있을 때 
+				dto.setFilename(fileName);
+				dto.setFilesize(fileSize);
+			}
+			
+			//첨부파일 삭제 처리
+			String fileDel = multi.getParameter("fileDel");
+			// 체크가 되지 않으면 null, 체크되면 on
+			if(fileDel != null && fileDel.equals("on")) 
+			{
+				// 테이블에 저장된 파일 이름
+				String fileName_d = dao.getFileName(num);
+				File f = new File(Constants.UPLOAD_PATH + fileName);
+				f.delete(); // 파일 삭제
+				
+				// 첨부파일 관련 레코드 정보 수정
+				dto.setFilename("-");
+				dto.setFilesize(0);
+				dto.setDown(0);
+			}
+			
+			// 
+			String result = dao.passwordCheck(num, password);
+			if(result != null) 
+			{ // 비밀번호가 맞을 경우
+				// dao에 update 요청
+				dao.update(dto);
+				// list.do로 이동
+				String page = request.getContextPath() + "/board_servlet/list.do";
+				response.sendRedirect(page);
+			} 
+			else 
+			{
+				request.setAttribute("dto", dto);
+				String page = "/board/board_edit.jsp?password_error=y";
+				RequestDispatcher rd = request.getRequestDispatcher(page);
+				rd.forward(request, response);
+			}
+		}
+		else if(url.indexOf("delete.do") != -1)
+		{
+			// enctype="multipart/form-data" 로 넘어온 값은 
+			// request 객체로 받을 수 없다.
+			MultipartRequest multi = new MultipartRequest(
+					request, 
+					Constants.UPLOAD_PATH, 
+					Constants.MAX_UPLOAD, "utf-8",
+					new DefaultFileRenamePolicy());
+			
+			int num = Integer.parseInt(multi.getParameter("num"));
+			dao.delete(num);
+			// DB 테이블 칼럽 추가 
+			// alter table board add shows char(1) default('y');
+			// 이럴경우 데이터를 완전 삭제가 아닌 shows 값을 n 으로 변경함으로 view 상에는 보이진 않지만
+			// 데이터는 존재하는 형태로 진행합니다.
+			
+			String page = new String();
+			page = request.getContextPath() + 
+					"/board_servlet/list.do";
 			response.sendRedirect(page);
 		}
 	}
--- 190529-게시판 만들기/src/board/dao/BoardDAO.java
+++ 190529-게시판 만들기/src/board/dao/BoardDAO.java
@@ -1,6 +1,10 @@
 package board.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -51,6 +55,169 @@ public class BoardDAO {
 		finally 
 		{
 			if(session != null) session.close(); // SqlSession 종료
+		}
+	}
+	
+	// 첨부파일
+	public String getFileName(int num) 
+	{
+		String result = new String();
+		SqlSession session = null;
+		
+		try 
+		{
+			// myvatis 실행 객체 생성
+			session = MyBatisManager.getInstance().openSession();
+			// 게시물 번호에 해당하는 첨부파일 이름을 리턴
+			result = session.selectOne("board.getFileName", num);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally 
+		{
+			if(session != null) session.close(); // SqlSession 종료
+		}
+		return result;
+	}
+	
+	// 다운로드 횟수 증가
+	public void plusDown(int num) 
+	{
+		SqlSession session = null;
+		try {
+			session = MyBatisManager.getInstance().openSession();
+			session.update("board.plusDown", num);
+			session.commit();
+		} 
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		} 
+		finally
+		{
+			if(session != null) session.close();
+		}
+	}
+	
+	// 조회수 증가
+	public void plusReadCount(int num, HttpSession count_session) 
+	{
+		SqlSession session = null;
+		try 
+		{
+			long read_time = 0; //게시물을 읽은 시간
+			
+			// 세션이 존재하면 읽은 시간을 가져옴
+			if(count_session.getAttribute("read_time_"+num) != null) 
+			{
+				read_time = (long)count_session.getAttribute("read_time_"+num);
+			}
+			long current_time = System.currentTimeMillis(); // 현재시각
+			session = MyBatisManager.getInstance().openSession();
+			// 일정시간이 경과하면 조회수를 올림
+			if(current_time - read_time > 5*1000) 
+			{
+				session.update("board.plusReadCount", num);
+				session.commit();
+				count_session.setAttribute("read_time_"+num, current_time);
+			}
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally 
+		{
+			if(session != null) session.close();
+		}
+	}
+	
+	// 상세화면 dto 리턴 코드
+	public BoardDTO view(int num, boolean newline) {
+		SqlSession session = null;
+		BoardDTO dto = null;
+		try {
+			session = MyBatisManager.getInstance().openSession();
+			dto = session.selectOne("board.view", num);
+			
+			// 줄바꿈 처리 
+			if(newline == true) {
+				String content = dto.getContent();
+				content = content.replace("\n", "<br/>");
+				dto.setContent(content);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null) session.close();
+		}
+		
+		return dto;
+	}
+	
+	// 게시물 비밀번호 체크	
+	public String passwordCheck(int num, String password) 
+	{
+		String result = null;
+		SqlSession session = null;
+		try
+		{
+			session = MyBatisManager.getInstance().openSession();
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("num", num);
+			map.put("password", password);
+			result = session.selectOne("board.pass_check", map);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally 
+		{
+			if(session != null) session.close();
+		}
+		return result;
+	}
+	
+	// 게시물 수정
+	public void update(BoardDTO dto) 
+	{
+		SqlSession session = null;
+		try 
+		{
+			session = MyBatisManager.getInstance().openSession();
+			session.update("board.update", dto);
+			session.commit();
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally 
+		{
+			if(session != null) session.close();
+		}
+	}
+	
+	// 게시물 삭제/숨김
+	public void delete(int num)
+	{
+		SqlSession session = null;
+		try 
+		{
+			session = MyBatisManager.getInstance().openSession();
+			session.update("board.delete", num);
+			session.commit();
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally 
+		{
+			if(session != null) session.close();
 		}
 	}
 }
